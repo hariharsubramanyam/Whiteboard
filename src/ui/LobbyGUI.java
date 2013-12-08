e092140d5c79ff17df05bb151b453a8fe152d195
Zeke Schmois <schmois@mit.edu>
2013-12-08 17:42:52 -0500

    Layout for lobbyGUI updated to include all comoponents. Still need to fix the null username on start up and the double clicking of boards. Also adding users to the list

--- src/ui/LobbyGUI.java
+++ src/ui/LobbyGUI.java
@@ -13,17 +13,18 @@ import javax.swing.DefaultListModel;
 import javax.swing.GroupLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JList;
 import javax.swing.JOptionPane;
 import javax.swing.JScrollPane;
 import javax.swing.JTextField;
 import javax.swing.SwingUtilities;
 
-import adts.Line;
-import adts.User;
 import protocol.Client;
 import protocol.ClientSideMessageMaker;
 import protocol.MessageHandler;
+import adts.Line;
+import adts.User;
 import canvas.Canvas;
 
 /**
@@ -44,146 +45,168 @@ import canvas.Canvas;
  *           500 pixels).
  * 
  */
-public class LobbyGUI extends JFrame implements Client{
+public class LobbyGUI extends JFrame implements Client {
 
 	private static final long serialVersionUID = 1L;
-	
+
 	// socket and its input and output streams
 	private Socket socket;
-    private PrintWriter out;
-    private BufferedReader in;
-    
-    // background thread to handle incoming messages
-    private final LobbyGUIBackgroundThread serverMessagesThread;
-    
-    // canvas which allows drawing on whiteboard
-    private Canvas canvas;
+	private PrintWriter out;
+	private BufferedReader in;
+
+	// background thread to handle incoming messages
+	private final LobbyGUIBackgroundThread serverMessagesThread;
+
+	// canvas which allows drawing on whiteboard
+	private Canvas canvas;
 
 	// UI elements for changing username
-	private final JTextField txtSetUserName;
+	private final JLabel labelUserName;
 	private final JButton btnSetUserName;
+	private final JButton btnCreateBoard;
 
 	// list of current boards
 	private final JList lstBoards;
 	private final JScrollPane scrollLstBoards;
 	private final DefaultListModel lstMdlBoards;
-	
-	// button to join board;
-	private final JButton btnJoinBoard;
-	
+
+	// list of all users
+	private final JList lstUsers;
+	private final JScrollPane scrollLstUsers;
+	private final DefaultListModel lstMdlUsers;
+
 	// layout manager
 	private final GroupLayout layout;
 
 	// self object
 	private final LobbyGUI self;
-	
+
 	// this user object
 	private User user;
-	
-	public LobbyGUI(){
-	    // get the hostname and create the socket
-	    try{
-	        String hostName = JOptionPane.showInputDialog("Enter the hostname of the whiteboard server:","localhost");
-    	    this.socket = new Socket(hostName, 4444);
-            this.out = new PrintWriter(socket.getOutputStream(), true);
-            this.in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-	    }catch(Exception ex){
-	        ex.printStackTrace();
-	    }
-	    
-	    // sets this current object
-	    this.self = this;
-            
-        // launch a thread to listen for messages
-        this.serverMessagesThread = new LobbyGUIBackgroundThread(this, this.in);
-        this.serverMessagesThread.start();
-        
-        // create the UI to view and change the username
-        this.txtSetUserName = new JTextField("No Username");
-        this.txtSetUserName.addActionListener(new SetUserNameListener());
-        this.btnSetUserName = new JButton("Change Username");
-        this.btnSetUserName.addActionListener(new SetUserNameListener());
-        
-        // create the list of boards 
-        this.lstMdlBoards = new DefaultListModel<String>();
-        this.lstMdlBoards.addElement("Create a new board...");
-        this.lstBoards = new JList<String>(this.lstMdlBoards);
-        this.lstBoards.setSelectedIndex(0);
-        this.scrollLstBoards = new JScrollPane(this.lstBoards);
-       
-        // join board button
-        this.btnJoinBoard = new JButton("Join Board");
-        this.btnJoinBoard.addActionListener(new JoinBoardListener());
-        
-        Container contentPane = this.getContentPane();
-        this.layout = new GroupLayout(contentPane);
-        contentPane.setLayout(this.layout);
-        this.layout.setAutoCreateGaps(true);
-        this.layout.setAutoCreateContainerGaps(true);
-        this.layout.setHorizontalGroup(this.layout
-                .createParallelGroup()
-                    .addGroup(this.layout
-                            .createSequentialGroup()
-                                .addComponent(this.txtSetUserName)
-                                .addComponent(this.btnSetUserName))
-                    .addComponent(this.scrollLstBoards)
-                    .addComponent(this.btnJoinBoard)
-                );
-        this.layout.setVerticalGroup(this.layout
-                .createSequentialGroup()
-                    .addGroup(this.layout
-                            .createParallelGroup()
-                                .addComponent(this.txtSetUserName,30,30,30)
-                                .addComponent(this.btnSetUserName))
-                    .addComponent(this.scrollLstBoards)
-                    .addComponent(this.btnJoinBoard)
-                );
-        
-        // Set title, size, and resizable
-        this.setTitle("Whiteboard Lobby");
-        this.setSize(300, 300);
-        this.setResizable(false);
-        
-        this.makeRequest(ClientSideMessageMaker.makeRequestStringGetBoardIDs());
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);   
+
+	public LobbyGUI() {
+		// get the hostname and create the socket
+		try {
+			String hostName = JOptionPane
+					.showInputDialog(
+							"Enter the hostname of the whiteboard server:",
+							"localhost");
+			this.socket = new Socket(hostName, 4444);
+			this.out = new PrintWriter(socket.getOutputStream(), true);
+			this.in = new BufferedReader(new InputStreamReader(
+					socket.getInputStream()));
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+
+		// sets this current object
+		this.self = this;
+
+		// launch a thread to listen for messages
+		this.serverMessagesThread = new LobbyGUIBackgroundThread(this, this.in);
+		this.serverMessagesThread.start();
+
+		// create the UI to view and change the username and new whiteboards
+		this.labelUserName = new JLabel("User: " + this.user);
+		this.btnSetUserName = new JButton("Change Username");
+		this.btnSetUserName.addActionListener(new SetUserNameListener());
+		this.btnCreateBoard = new JButton("Create Whiteboard");
+		this.btnCreateBoard.addActionListener(new CreateWhiteboardListener());
+
+		// create the list of boards
+		this.lstMdlBoards = new DefaultListModel<String>();
+		this.lstBoards = new JList<String>(this.lstMdlBoards);
+		this.lstBoards.setSelectedIndex(0);
+		this.scrollLstBoards = new JScrollPane(this.lstBoards);
+
+		// create the list of users
+		this.lstMdlUsers = new DefaultListModel<String>();
+		this.lstUsers = new JList<String>(this.lstMdlUsers);
+		this.lstUsers.setSelectedIndex(0);
+		this.scrollLstUsers = new JScrollPane(this.lstUsers);
+
+		Container contentPane = this.getContentPane();
+		this.layout = new GroupLayout(contentPane);
+		contentPane.setLayout(this.layout);
+		this.layout.setAutoCreateGaps(true);
+		this.layout.setAutoCreateContainerGaps(true);
+		this.layout
+				.setHorizontalGroup(this.layout
+						.createParallelGroup()
+						.addGroup(
+								this.layout
+										.createSequentialGroup()
+										.addComponent(this.labelUserName, 100,
+												100, 100)
+										.addComponent(this.btnCreateBoard)
+										.addComponent(this.btnSetUserName))
+						.addGroup(
+								this.layout
+										.createSequentialGroup()
+										.addComponent(this.scrollLstBoards,
+												250, 250, 250)
+										.addComponent(this.scrollLstUsers))
+
+				);
+		this.layout.setVerticalGroup(this.layout
+				.createSequentialGroup()
+				.addGroup(
+						this.layout.createParallelGroup()
+								.addComponent(this.labelUserName)
+								.addComponent(this.btnCreateBoard)
+								.addComponent(this.btnSetUserName))
+				.addGroup(
+						this.layout.createParallelGroup()
+								.addComponent(this.scrollLstBoards)
+								.addComponent(this.scrollLstUsers)));
+
+		// Set title, size, and resizable
+		this.setTitle("Whiteboard Lobby");
+		this.setSize(500, 300);
+		this.setResizable(false);
+
+		this.makeRequest(ClientSideMessageMaker.makeRequestStringGetBoardIDs());
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
-	
-	public void makeRequest(String req){
-	    out.println(req);
-	    System.out.println("REQ: " + req);
+
+	public void makeRequest(String req) {
+		out.println(req);
+		System.out.println("REQ: " + req);
 	}
-	
-	public void onReceiveBoardIDs(List<Integer> rcvdIDs){
-	    final List<Integer> boardIDs = rcvdIDs;
-	    SwingUtilities.invokeLater(new Thread(){
-	        @Override
-	        public void run() {
-	            lstMdlBoards.clear();
-	            for(int boardID : boardIDs){
-	                lstMdlBoards.addElement("Board " + boardID);
-	            }
-                lstMdlBoards.addElement("Create a new board...");
-                lstBoards.setSelectedIndex(lstMdlBoards.size()-1);
-	        }
-	    });
+
+	public void onReceiveBoardIDs(List<Integer> rcvdIDs) {
+		final List<Integer> boardIDs = rcvdIDs;
+		SwingUtilities.invokeLater(new Thread() {
+			@Override
+			public void run() {
+				lstMdlBoards.clear();
+				for (int boardID : boardIDs) {
+					lstMdlBoards.addElement("Board " + boardID);
+				}
+				lstMdlBoards.addElement("Create a new board...");
+				lstBoards.setSelectedIndex(lstMdlBoards.size() - 1);
+			}
+		});
 	}
-	
-	public void onReceiveUsernameChanged(String rcvdName){
-	    final String newName = rcvdName;
-        SwingUtilities.invokeLater(new Thread(){
-            @Override
-            public void run() {
-                txtSetUserName.setText(newName);
-                JOptionPane.showMessageDialog(null, "Changed username to " + newName);
-            }
-        });
+
+	public void onReceiveUsernameChanged(String rcvdName) {
+		final String newName = rcvdName;
+		SwingUtilities.invokeLater(new Thread() {
+			@Override
+			public void run() {
+				user.setName(newName);
+				labelUserName.setText(newName);
+				JOptionPane.showMessageDialog(null, "Changed username to "
+						+ newName);
+			}
+		});
 	}
-	
-	public void onReceiveWelcome(int id){
-	    System.out.println("Here");
-	    this.user = new User(id);
+
+	public void onReceiveWelcome(int id) {
+		System.out.println("Here");
+		this.user = new User(id);
 	}
-	
+
 	/**
 	 * This is the main function.
 	 * 
@@ -199,49 +222,65 @@ public class LobbyGUI extends JFrame implements Client{
 		});
 	}
 
-	
-	private class SetUserNameListener implements ActionListener{
-        @Override
-        public void actionPerformed(ActionEvent e) {
-            user.setName(txtSetUserName.getText());
-            String userName = user.getName();
-            makeRequest(ClientSideMessageMaker.makeRequestStringSetUsername(userName));
-        }
-	}
-	
-	private class JoinBoardListener implements ActionListener{
-        @Override
-        public void actionPerformed(ActionEvent e) {
-            String selectedItem = (String)lstBoards.getSelectedValue();
-            canvas = new Canvas(1000, 1000, self, user.getName());
-            canvas.setVisible(true);
-            setVisible(false);
-            if(selectedItem.equals("Create a new board...")){
-                out.println(MessageHandler.makeRequestStringCreateBoard("MyBoard"));
-            }else{
-                out.println(MessageHandler.makeRequestStringJoinBoardID(Integer.parseInt(selectedItem.replace("Board ", ""))));
-            }
-            
-        }
+	private class SetUserNameListener implements ActionListener {
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			String newUser = JOptionPane.showInputDialog("Enter new user name");
+			user.setName(newUser);
+			String userName = user.getName();
+			makeRequest(ClientSideMessageMaker
+					.makeRequestStringSetUsername(userName));
+		}
 	}
 
-    @Override
-    public void onReceiveDraw(Line l) {
-        canvas.onReceiveDraw(l);
-    }
+	private class CreateWhiteboardListener implements ActionListener {
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			String newBoard = JOptionPane
+					.showInputDialog("Enter new Whiteboard name");
+			canvas = new Canvas(1000, 1000, self, user.getName());
+			canvas.setVisible(true);
+			setVisible(false);
+			out.println(ClientSideMessageMaker
+					.makeRequestStringCreateBoard(newBoard));
+		}
+	}
 
-    @Override
-    public void onReceiveBoardLines(List<Line> ls) {
-        canvas.onReceiveBoardLines(ls);
-    }
+	private class JoinBoardListener implements ActionListener {
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			String selectedItem = (String) lstBoards.getSelectedValue();
+			canvas = new Canvas(1000, 1000, self, user.getName());
+			canvas.setVisible(true);
+			setVisible(false);
+			if (selectedItem.equals("Create a new board...")) {
+				out.println(MessageHandler
+						.makeRequestStringCreateBoard("MyBoard"));
+			} else {
+				out.println(MessageHandler.makeRequestStringJoinBoardID(Integer
+						.parseInt(selectedItem.replace("Board ", ""))));
+			}
 
-    @Override
-    public void onReceiveClear() {
-        canvas.onReceiveClear();
-    }
+		}
+	}
 
-    @Override
-    public void onReceiveUsers(List<String> users) {
-        canvas.onReceiveUsers(users);
-    }
+	@Override
+	public void onReceiveDraw(Line l) {
+		canvas.onReceiveDraw(l);
+	}
+
+	@Override
+	public void onReceiveBoardLines(List<Line> ls) {
+		canvas.onReceiveBoardLines(ls);
+	}
+
+	@Override
+	public void onReceiveClear() {
+		canvas.onReceiveClear();
+	}
+
+	@Override
+	public void onReceiveUsers(List<String> users) {
+		canvas.onReceiveUsers(users);
+	}
 }
