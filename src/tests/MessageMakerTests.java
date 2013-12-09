10b6a3b8908a148ac31b071dbfe14fd73941f19e
Robert Perez <r_perez@mit.edu>
2013-12-09 09:43:22 -0500

    Updates to tests

---
+++ src/tests/MessageMakerTests.java
@@ -0,0 +1,29 @@
+package tests;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+import protocol.ClientSideMessageMaker;
+
+
+
+
+/**
+ * Description of test suite (what it tests, etc.) TODO
+ */
+public class MessageMakerTests {
+	
+	/*
+	 * Testing strategy
+	 * 
+	 * Goal: Check that ClientSideMessageMaker makes the right messages.
+	 * 
+	 * Strategy: Test all message formats that it's expected to create.
+	 *           
+	 */
+	
+	@Test
+	public void create_board_test() {
+		
+	}
+}
